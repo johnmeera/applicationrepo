62719b540d772bcef0f2fd3d30fbdc5375dd95e1
johnmeera <143019004+johnmeera@users.noreply.github.com>
2023-09-02 16:47:03 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "example_buckets" {
-  bucket        = "mybuckename"
-  acl = "private"
+  bucket        = "var.mybuckename"
+  acl = "var.myname"
   force_destroy = true
 }
