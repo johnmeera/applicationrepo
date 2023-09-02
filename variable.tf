15c623d6adfb152fe2b0f3f18804bf9f57101e37
johnmeera <143019004+johnmeera@users.noreply.github.com>
2023-09-02 16:17:35 +0530

    variable.tf

--- variable.tf
+++ variable.tf
@@ -1,10 +1,1 @@
-variable "mybucketname" {
-  description = "bucket name my wish"
-  type        =  string
 
-}
-variable "myname" {
-  description = "bucket name my wish"
-  type        = string
-
-}
